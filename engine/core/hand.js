cec32fa08a4bdb9a88b8bf1f6fdaf17a33f3b76c
Didictateur <adrien.decosse@gmail.com>
2025-10-15 09:23:03 +0200

    template for cards

---
+++ engine/core/hand.js
@@ -0,0 +1,29 @@
+import Card from './card.js';
+
+class Hand {
+    constructor() {
+        /** @type {Array(Card)} */
+        this.cards = [];
+    }
+
+    /**
+     * @param {Card} card 
+     */
+    pushCard(card) {
+        this.cards.push(card);
+    }
+
+    /**
+     * @param {number} index
+     * @returns {Card|null}
+     */
+    popCard(index) {
+        if (index < 0 || index >= this.cards.length) {
+            return null;
+        } else {
+            let c = this.cards.at(index);
+            this.cards.splice(index, 1);
+            return c;
+        }
+    }
+}
